a6f4a8c339b0a1266767d101b269d138af5f4789
harinisf <121146560+harinisf@users.noreply.github.com>
2023-09-15 15:10:21 +0530

    modified the content in changing header styles using methods contents

---
+++ ej2-javascript/code-snippet/grid/column-header-tooltip/index.ts
@@ -0,0 +1,62 @@
+import { Grid, Page, Selection} from '@syncfusion/ej2-grids';
+import { Tooltip, TooltipEventArgs } from '@syncfusion/ej2-popups';
+import { data } from './datasource.ts';
+
+Grid.Inject(Page, Selection);
+
+let grid: Grid = new Grid({
+  dataSource: data,
+  allowPaging: true,
+  pageSettings: { pageCount: 5 },
+  columns: [
+    {
+      field: 'OrderID',
+      headerText: 'Order ID',
+      textAlign: 'Right',
+      width: 120,
+    },
+
+    {
+      field: 'Freight',
+      headerText: 'Freight',
+      textAlign: 'Right',
+      width: 120,
+      format: 'C2',
+    },
+
+    {
+      field: 'ShipName',
+      headerText: 'Ship Name',
+      textAlign: 'Right',
+      width: 145,
+      format: 'yMd',
+    },
+    {
+      field: 'ShipCountry',
+      headerText: 'Ship Country',
+      width: 140,
+    },
+  ],
+});
+grid.appendTo('#Grid');
+
+let columnDescriptions: Object = {
+  'Order ID': 'A unique number assigned to each order.',
+  'Freight' : 'The cost of shipping the order.',
+  'Ship Name': 'The name of the person or company who will receive the shipment.',
+  'Ship Country': 'The country to which the shipment will be sent.',
+  'Order Date': 'The date when the order was placed.',
+};
+
+var tooltip = new Tooltip({
+  beforeRender: beforeRender,
+  target: '.e-headertext',
+});
+tooltip.appendTo('#tooltip');
+function beforeRender(args: TooltipEventArgs) {
+  const description = (columnDescriptions as Object)[args.target.innerText];
+  if (description) {
+    (tooltip as Tooltip).content =
+      (args as any).target.innerText + ': ' + description;
+  }
+}
