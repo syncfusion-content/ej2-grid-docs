92038be2deb57bdce8e1a6a98c3b51635e4e0048
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2023-09-22 09:57:52 +0530

    Update the sample

--- ej2-javascript/code-snippet/grid/detail-template-childgrid-cs1/index.js
+++ ej2-javascript/code-snippet/grid/detail-template-childgrid-cs1/index.js
@@ -1,6 +1,6 @@
 ej.grids.Grid.Inject(ej.grids.DetailRow);
 var grid = new ej.grids.Grid({
-    dataSource: employeeData.slice(2, 5),
+    dataSource: employeeData,
     detailTemplate: '#detailtemplate',
     columns: [
         { field: 'FirstName', headerText: 'First Name', width: 140 },
@@ -8,16 +8,16 @@ var grid = new ej.grids.Grid({
         { field: 'Title', headerText: 'Title', width: 150 },
         { field: 'Country', headerText: 'Country', width: 150 }
     ],
-    detailDataBound: function(e){
+    detailDataBound: function(DetailDataBoundEventArgs){
         var detail = new ej.grids.Grid({
-            dataSource: data.filter(function(item){return item['EmployeeID'] === e.data['EmployeeID'];}).slice(0, 3),
+            dataSource: data.filter(function(item){ return item['EmployeeID'] === DetailDataBoundEventArgs.data['EmployeeID'];}),
             columns: [
                 { field: 'OrderID', headerText: 'Order ID', width: 110 },
                 { field: 'CustomerID', headerText: 'Customer Name', width: 140 },
                 { field: 'ShipCountry', headerText: 'Ship Country', width: 150 }
             ]
         });
-        detail.appendTo(e.detailElement.querySelector('.custom-grid'));
+        detail.appendTo(DetailDataBoundEventArgs.detailElement.querySelector('.custom-grid'));
     }
 });
 grid.appendTo('#Grid');
