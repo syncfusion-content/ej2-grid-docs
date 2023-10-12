84ceb49ccf7a79e0dd9954df531950687e05cd11
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2023-10-12 14:30:37 +0530

    update the version

--- ej2-javascript/code-snippet/grid/row-template-component-cs1/index.ts
+++ ej2-javascript/code-snippet/grid/row-template-component-cs1/index.ts
@@ -11,48 +11,47 @@ let grid: Grid = new Grid({
     columns: [
         { field: 'OrderID', headerText: 'Order ID', width: 120 },
         { field: 'Quantity', headerText: 'Quantity', width: 170 },
-        { field: 'ShipAddress', headerText: 'ShipAddress', width: 170 },
+        { field: 'ShipAddress', headerText: 'Ship Address', width: 170 },
         { field: 'OrderDate', headerText: 'Order Date', width: 120 },
         { field: 'OrderStatus', headerText: 'Order Status', width: 120 },
     ],
 
     dataBound: (args) => {
 
-        var gridInstance = (document.getElementById('Grid') as any).ej2_instances[0];
+        let gridInstance = (document.getElementById('Grid') as any).ej2_instances[0];
 
-        var chipList = gridInstance.getContentTable().querySelectorAll('.chipList');
-        for (var i = 0; i < chipList.length; i++) {
-            var chipValue  = chipList[i].innerText;
+        let chipList = gridInstance.getContentTable().querySelectorAll('.chipList');
+        for (let i = 0; i < chipList.length; i++) {
+            let chipValue  = chipList[i].innerText;
             new ChipList({ chips: [chipValue] }, chipList[i]);
         }
 
-        var NumericList = gridInstance.getContentTable().querySelectorAll('.numeric');
-        for (var i = 0; i < NumericList.length; i++) {
+        let NumericList = gridInstance.getContentTable().querySelectorAll('.numeric');
+        for (let i = 0; i < NumericList.length; i++) {
             let numeric: NumericTextBox = new NumericTextBox({});
             numeric.appendTo(NumericList[i]);
         }
 
-        var dateList = gridInstance.getContentTable().querySelectorAll('.date-input');
-        for (var i = 0; i < dateList.length; i++) {
-            var dateInput = dateList[i];
-            var dateValue = dateInput.value;
+        let dateList = gridInstance.getContentTable().querySelectorAll('.date-input');
+        for (let i = 0; i < dateList.length; i++) {
+            let dateInput = dateList[i];
+            let dateValue = dateInput.value;
             let datepickerObject: DatePicker = new DatePicker({
                 value: new Date(dateValue),
             });
             datepickerObject.appendTo(dateInput);
         }
         
-        var dropdownList = gridInstance.getContentTable().querySelectorAll('.dropdownlist-input');
-        for (var i = 0; i < dropdownList.length; i++) {
-            var dropdownInputValue = dropdownList[i];
-            var dropData = ['Processing', 'Order Placed', 'Delivered'];
-            var dropdown = new DropDownList({
+        let dropdownList = gridInstance.getContentTable().querySelectorAll('.dropdownlist-input');
+        for (let i = 0; i < dropdownList.length; i++) {
+            let dropdownInputValue = dropdownList[i];
+            let dropData = ['Processing', 'Order Placed', 'Delivered'];
+            let dropdown = new DropDownList({
                 dataSource: dropData,
                 value: dropdownList[i].value,
                 popupHeight: 150,
                 popupWidth: 150
             });
-
             dropdown.appendTo(dropdownInputValue);
         }
     },
