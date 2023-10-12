800fa4c1b0ee21fb24be123c7e226e1e850252e4
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2023-10-12 12:11:10 +0530

    Documentation(846855)- modified the detail-template

--- ej2-javascript/code-snippet/grid/detail-template-childgrid-cs1/index.ts
+++ ej2-javascript/code-snippet/grid/detail-template-childgrid-cs1/index.ts
@@ -6,7 +6,7 @@ import { employeeData, data } from './datasource.ts';
 Grid.Inject(DetailRow);
 
 let grid: Grid = new Grid({
-    dataSource: employeeData.slice(2, 5),
+    dataSource: employeeData,
     detailTemplate: '#detailtemplate',
     columns: [
         { field: 'FirstName', headerText: 'First Name', width: 140 },
@@ -16,7 +16,7 @@ let grid: Grid = new Grid({
     ],
     detailDataBound: (e: DetailDataBoundEventArgs) => {
         let detail = new Grid({
-            dataSource: data.filter((item: Object) => item['EmployeeID'] === e.data['EmployeeID']).slice(0, 3),
+            dataSource: data.filter((item: Object) => item['EmployeeID'] === e.data['EmployeeID']),
             columns: [
                 { field: 'OrderID', headerText: 'Order ID', width: 110 },
                 { field: 'CustomerID', headerText: 'Customer Name', width: 140 },
