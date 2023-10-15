ccfc681e9feb4be82fb89bc722e8bca708de747a
harinisf <121146560+harinisf@users.noreply.github.com>
2023-10-15 21:52:20 +0530

    modified samples and conents in javascript and typecript

---
+++ ej2-javascript/code-snippet/grid/grid-cs82-grouped/index.js
@@ -0,0 +1,49 @@
+ej.grids.Grid.Inject(ej.grids.Sort, ej.grids.Group);
+
+var grid = new ej.grids.Grid({
+  dataSource: data,
+  allowGrouping: true,
+  groupSettings: { columns: ['CustomerID'] },
+
+  columns: [
+    {
+      field: 'OrderID',
+      headerText: 'Order ID',
+      textAlign: 'Right',
+      width: 90,
+      allowGrouping: false,
+    },
+    { field: 'CustomerID', headerText: 'Customer ID', width: 100 },
+    {
+      field: 'ShipCity',
+      headerText: 'Ship City',
+      width: 100,
+      allowGrouping: false,
+    },
+    {
+      field: 'ShipName',
+      headerText: 'Ship Name',
+      width: 120,
+      allowGrouping: false,
+    },
+  ],
+  height: 240,
+});
+grid.appendTo('#Grid');
+let message = document.getElementById('message');
+document.getElementById('button').onclick = () => {
+  const groupedRows = Array.from(
+    grid
+      .getContentTable()
+      .querySelectorAll('.e-recordplusexpand, .e-recordpluscollapse')
+  );
+  let groupedRowIndex = parseInt(document.getElementById('rowIndex').value);
+  if (groupedRows.length >= 0 && groupedRowIndex < groupedRows.length) {
+    message.innerHTML = '';
+    let groupCaptionElement = groupedRows[groupedRowIndex];
+    grid.groupModule.expandCollapseRows(groupCaptionElement);
+  } else {
+    message.innerHTML =
+      'The entered index exceeds the total number of grouped rows. Please enter a valid grouped index.';
+  }
+};
--- ej2-javascript/code-snippet/grid/grid-cs84/index.js
+++ ej2-javascript/code-snippet/grid/grid-cs84/index.js
@@ -1,17 +1,52 @@
-var grid = new ej.grids.Grid({
-    dataSource: data,
-    allowGrouping: true,
-    allowSorting: true,
-    sortSettings: { columns: [{ field: 'CustomerID', direction: 'Descending' }] },
-    groupSettings: { columns: ['CustomerID'] },
-    columns: [
-        { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 120 },
-        { field: 'CustomerID', headerText: 'Customer ID', width: 150 },
-        { field: 'ShipCity', headerText: 'Ship City', width: 150 },
-        { field: 'ShipName', headerText: 'Ship Name', width: 150 }
-    ],
-    height: 267
-});
+ej.grids.Grid.Inject(ej.grids.Sort, ej.grids.Group);
 
+var grid = new ej.grids.Grid({
+  dataSource: data,
+  allowGrouping: true,
+  allowSorting: true,
+  groupSettings: { showDropArea: false, columns: ['CustomerID', 'ShipName'] },
+  columns: [
+    { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 90 },
+    { field: 'CustomerID', headerText: 'Customer ID', width: 100 },
+    { field: 'ShipCity', headerText: 'Ship City', width: 100 },
+    { field: 'ShipName', headerText: 'Ship Name', width: 120 },
+  ],
+  height: 315,
+});
 grid.appendTo('#Grid');
 
+let columns = [
+  { text: 'CustomerID', value: 'CustomerID' },
+  { text: 'OrderID', value: 'OrderID' },
+  { text: 'ShipCity', value: 'ShipCity' },
+  { text: 'ShipName', value: 'ShipName' },
+];
+
+let field = { text: 'text', value: 'value' };
+
+let dropdown = new ej.dropdowns.DropDownList({
+  value: 'OrderID',
+  popupHeight: '240px',
+  width: 120,
+  dataSource: columns,
+  field: field,
+});
+dropdown.appendTo('#dropdowns');
+
+button = new ej.buttons.Button({
+  content: 'Group Column',
+});
+button.appendTo('#group');
+
+document.getElementById('group').onclick = () => {
+  grid.groupColumn(dropdown.value);
+};
+
+button = new ej.buttons.Button({
+  content: 'Ungroup Column',
+});
+button.appendTo('#ungroup');
+
+document.getElementById('ungroup').onclick = () => {
+  grid.ungroupColumn(dropdown.value);
+};
