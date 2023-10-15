02e84093572e05b9ee7fd6c565e44c9f34bab09d
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2023-10-15 09:37:06 +0530

    Documentation(844955) - modified the column-spanning in column and update the sample

--- ej2-javascript/code-snippet/grid/column-spanning-cs1/index.ts
+++ ej2-javascript/code-snippet/grid/column-spanning-cs1/index.ts
@@ -1,6 +1,6 @@
 
 
-import { Grid } from '@syncfusion/ej2-grids';
+import { Grid,  QueryCellInfoEventArgs } from '@syncfusion/ej2-grids';
 import { columnSpanData, ColumnSpanDataType  } from './datasource.ts';
 
 let grid: Grid = new Grid({
