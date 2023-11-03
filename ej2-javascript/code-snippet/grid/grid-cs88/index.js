74e91f78cdfabb577b9b8b26d47c20c9d7ae7702
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2023-11-03 18:59:14 +0530

    Documentation(851668) - Modified the hierarchy grid

--- ej2-javascript/code-snippet/grid/grid-cs88/index.js
+++ ej2-javascript/code-snippet/grid/grid-cs88/index.js
@@ -2,19 +2,19 @@ ej.grids.Grid.Inject(ej.grids.DetailRow);
 var grid = new ej.grids.Grid({
     dataSource: employeeData,
     columns: [
-        { field: 'EmployeeID', headerText: 'Employee ID', textAlign: 'Right', width: 120 },
-        { field: 'FirstName', headerText: 'First Name', width: 150 },
-        { field: 'City', headerText: 'City', width: 150 },
-        { field: 'Country', headerText: 'Country', width: 150 }
+        { field: 'EmployeeID', headerText: 'Employee ID', textAlign: 'Right', width: 100 },
+        { field: 'FirstName', headerText: 'First Name', width: 100 },
+        { field: 'LastName', headerText: 'LastName', width: 100 },
+        { field: 'City', headerText: 'City', width: 120 }
     ],
     childGrid: {
         dataSource: childdata,
         queryString: 'ID',
         columns: [
-            { field: 'ID', headerText: 'Order ID', textAlign: 'Right', width: 120 },
-            { field: 'CustomerID', headerText: 'Customer ID', width: 150 },
-            { field: 'ShipCity', headerText: 'Ship City', width: 150 },
-            { field: 'ShipName', headerText: 'Ship Name', width: 150 }
+            { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 90 },
+            { field: 'CustomerID', headerText: 'Customer ID', width: 100 },
+            { field: 'ShipCity', headerText: 'Ship City', width: 100 },
+            { field: 'ShipName', headerText: 'Ship Name', width: 100 }
         ],
         load: load,
     },
@@ -25,4 +25,3 @@ grid.appendTo('#Grid');
 function load(args) {
       this.parentDetails.parentKeyFieldValue = this.parentDetails.parentRowData['EmployeeID'];
 }
-
