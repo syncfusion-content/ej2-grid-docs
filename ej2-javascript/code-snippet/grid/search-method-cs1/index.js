b30a9b5d629dd7d816b662cf53fd75df45d4384f
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2023-11-03 16:00:34 +0530

    Documentation(850296) - Modified the searching for javascript and typescript

--- ej2-javascript/code-snippet/grid/search-method-cs1/index.js
+++ ej2-javascript/code-snippet/grid/search-method-cs1/index.js
@@ -1,10 +1,10 @@
 var grid = new ej.grids.Grid({
     dataSource: data,
     columns: [
-        { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 120 },
-        { field: 'CustomerID', headerText: 'Customer ID', width: 150 },
-        { field: 'ShipCity', headerText: 'Ship City', width: 150 },
-        { field: 'ShipName', headerText: 'Ship Name', width: 150 }
+        { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 90 },
+        { field: 'CustomerID', headerText: 'Customer ID', width: 100 },
+        { field: 'ShipCity', headerText: 'Ship City', width: 100 },
+        { field: 'ShipName', headerText: 'Ship Name', width: 120 }
     ],
     height: 260
 });
@@ -17,4 +17,3 @@ document.getElementById('search').addEventListener('click', function() {
     var searchText = document.getElementsByClassName('searchtext')[0].value;
     grid.search(searchText);
 });
-
