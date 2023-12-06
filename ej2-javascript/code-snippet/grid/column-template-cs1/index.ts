a5f5014211267f2d11a1ae1bd44ce676da920342
harinisf <121146560+harinisf@users.noreply.github.com>
2023-12-06 17:14:21 +0530

    Ug-Documentation-Column-Template

--- ej2-javascript/code-snippet/grid/column-template-cs1/index.ts
+++ ej2-javascript/code-snippet/grid/column-template-cs1/index.ts
@@ -1,22 +1,14 @@
-
-
 import { Grid } from '@syncfusion/ej2-grids';
-import { employeeData } from './datasource.ts';
+import { employeeData } from './datasource';
 
 let grid: Grid = new Grid({
     dataSource: employeeData,
     columns: [
-        {
-            headerText: 'Employee Image', textAlign: 'Center',
-            template: '#template', width: 150
-        },
-        { field: 'EmployeeID', headerText: 'Employee ID', textAlign: 'Right', width: 125 },
-        { field: 'FirstName', headerText: 'Name', width: 120 },
-        { field: 'Title', headerText: 'Title', width: 170 }
+        { headerText: 'Employee Image', textAlign: 'Center', template: '#template', width: 150 },
+        { field: 'FirstName', headerText: 'First Name', width: 100 },
+        { field: 'LastName', headerText: 'Last Name', width: 100 },
+        { field: 'City', headerText: 'City', textAlign: 'Right', width: 100 }
     ],
     height: 315
 });
 grid.appendTo('#Grid');
-
-
-
