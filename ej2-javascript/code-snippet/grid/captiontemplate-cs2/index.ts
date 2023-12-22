f9f1441c763b1de1a7fae8123f4c753f386e983d
harinisf <121146560+harinisf@users.noreply.github.com>
2023-12-22 16:42:28 +0530

    EJ2-Documentation-849998-Ug-Documentation-For-Caption-Template

--- ej2-javascript/code-snippet/grid/captiontemplate-cs2/index.ts
+++ ej2-javascript/code-snippet/grid/captiontemplate-cs2/index.ts
@@ -1,17 +1,29 @@
 import { Grid, Group } from '@syncfusion/ej2-grids';
+import { L10n,setCulture} from '@syncfusion/ej2-base';
 import { data } from './datasource.ts';
 
 Grid.Inject(Group);
+setCulture('ar');
+
+L10n.load({
+    ar: {
+      grid: {
+        GroupDropArea: 'اسحب رأس العمود هنا لتجميع العمود',
+        Item: 'بند',
+        Items: 'العناصر',
+        GroupCaption: ' هي خلية تسمية توضيحية جماعية',
+      },
+    },
+});
 
 let grid: Grid = new Grid({
     dataSource: data,
     allowGrouping: true,
-    groupSettings: { captionTemplate: '#captiontemplate' },
     columns: [
         { field: 'OrderID', headerText: 'ID',textAlign: 'Right',width: 90 },
         { field: 'CustomerID', headerText: 'Name',width: 100 },
         { field: 'ShipCity', headerText: 'City', width: 100 },
-        { field: 'Freight', headerText: 'Value', width: 80 }
+        { field: 'ShipName', headerText: 'Value', width: 80 }
     ],
     height: 315
 });
