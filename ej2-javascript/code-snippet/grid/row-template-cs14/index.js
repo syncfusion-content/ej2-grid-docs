ac5bfb41a186969673a34b660aacf54f8c3066cd
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2024-01-03 18:42:23 +0530

    EJ2-Documentation(856619)- UG Documentation of Context-menu

--- ej2-javascript/code-snippet/grid/row-template-cs14/index.js
+++ ej2-javascript/code-snippet/grid/row-template-cs14/index.js
@@ -1,22 +1,19 @@
-ej.grids.Grid.Inject(ej.grids.Page, ej.grids.Group, ej.grids.Sort, ej.grids.Filter, ej.grids.ContextMenu);
+ej.grids.Grid.Inject(ej.grids.Page, ej.grids.Group, ej.grids.Sort, ej.grids.Edit, ej.grids.ContextMenu, ej.grids.PdfExport,  ej.grids.ExcelExport);
 var grid = new ej.grids.Grid({    
     dataSource: data,
     allowSorting: true,
     allowPaging: true,
-    editSettings: {allowEditing: true, allowAdding: true},
+    allowGrouping:true,
+    editSettings: {allowEditing: true, allowDeleting: true},
     allowPdfExport: true,
     allowExcelExport: true,
-    contextMenuItems: ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending',
-                'Copy', 'Edit', 'Delete', 'Save', 'Cancel',
-                'PdfExport', 'ExcelExport', 'CsvExport', 'FirstPage', 'PrevPage',
-                'LastPage', 'NextPage'],
+    contextMenuItems: ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending', 'Copy', 'Edit', 'Delete', 'Save', 'Cancel','PdfExport', 'ExcelExport', 'CsvExport', 'FirstPage', 'PrevPage', 'LastPage', 'NextPage', 'Group', 'Ungroup'],
     columns: [
-        { field: 'OrderID', headerText: 'Order ID', width: 200, textAlign: 'Right'},
-        { field: 'Freight', width: 150, format: 'C2', textAlign: 'Right', editType: 'numericedit' },
-        { field: 'ShipName', headerText: 'Ship Name', width: 300 },
-        { field: 'ShipCountry', headerText: 'Ship Country',  width: 200 },
-        { field: 'ShipCity', headerText: 'Ship City', width: 200 }
+        { field: 'OrderID', headerText: 'Order ID', width: 150, textAlign: 'Right', isPrimaryKey:true},
+        { field: 'CustomerID', headerText: 'Customer Name', width: 150 },
+        { field: 'Freight', headerText:'Freight', width: 150, format: 'C2', textAlign: 'Right', editType: 'numericedit' },
+        { field: 'ShipCity', headerText: 'Ship City', width: 200 },
+        { field: 'ShipName', headerText: 'Ship Name', width: 300 }
     ]
 });
 grid.appendTo('#Grid');
-
--- ej2-javascript/code-snippet/grid/row-template-cs14/index.ts
+++ ej2-javascript/code-snippet/grid/row-template-cs14/index.ts
@@ -1,5 +1,3 @@
-
-
 import { Grid, Resize, Sort, Group, ContextMenu, Edit, Page, PdfExport, ExcelExport } from '@syncfusion/ej2-grids';
 import { data  } from './datasource.ts';
 
@@ -9,23 +7,17 @@ let grid: Grid = new Grid({
     dataSource: data,
     allowSorting: true,
     allowPaging: true,
-    editSettings: {allowEditing: true, allowDeleting: true},
+    allowGrouping:true,
+    editSettings: {allowEditing: true, allowDeleting: true },
     allowPdfExport: true,
     allowExcelExport: true,
-    contextMenuItems: ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending',
-                'Copy', 'Edit', 'Delete', 'Save', 'Cancel',
-                'PdfExport', 'ExcelExport', 'CsvExport', 'FirstPage', 'PrevPage',
-                'LastPage', 'NextPage'],
+    contextMenuItems: ['AutoFit', 'AutoFitAll', 'SortAscending', 'SortDescending', 'Copy', 'Edit', 'Delete', 'Save', 'Cancel', 'PdfExport', 'ExcelExport', 'CsvExport', 'FirstPage', 'PrevPage', 'LastPage', 'NextPage', 'Group', 'Ungroup'],
     columns: [
-        { field: 'OrderID', headerText: 'Order ID', width: 200, textAlign: 'Right'},
-        { field: 'Freight', width: 150, format: 'C2', textAlign: 'Right', editType: 'numericedit' },
-        { field: 'ShipName', headerText: 'Ship Name', width: 300 },
-        { field: 'ShipCountry', headerText: 'Ship Country',  width: 200 },
-        { field: 'ShipCity', headerText: 'Ship City', width: 200 }
+        { field: 'OrderID', headerText: 'Order ID', width: 150, textAlign: 'Right', isPrimaryKey:true},
+        { field: 'CustomerID', headerText: 'Customer Name', width: 150 },
+        { field: 'Freight', headerText:'Freight', width: 150, format: 'C2', textAlign: 'Right', editType: 'numericedit' },
+        { field: 'ShipCity', headerText: 'Ship City', width: 200 },
+        { field: 'ShipName', headerText: 'Ship Name', width: 300 }
     ]
 });
 grid.appendTo('#Grid');
-
-
-
-
