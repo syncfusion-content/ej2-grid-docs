1307adcb30ef2eccd794e65e9d1128e1fa02eb3a
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2024-01-03 16:18:15 +0530

    Ej2-Documentation(850296) - UG Documentation of Searching

--- ej2-javascript/code-snippet/grid/column-search-cs1/index.ts
+++ ej2-javascript/code-snippet/grid/column-search-cs1/index.ts
@@ -1,5 +1,3 @@
-
-
 import { Grid, Toolbar } from '@syncfusion/ej2-grids';
 import { data } from './datasource.ts';
 
@@ -9,15 +7,12 @@ let grid: Grid = new Grid({
     dataSource: data,
     searchSettings: { fields: ['CustomerID', 'ShipCity', 'ShipName']},
     columns: [
-        { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 100 },
-        { field: 'CustomerID', headerText: 'Customer ID', width: 120 },
-        { field: 'ShipCity', headerText: 'Ship City', width: 100 },
-        { field: 'ShipName', headerText: 'Ship Name', width: 100 }
+        { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 90 },
+        { field: 'CustomerID', headerText: 'Customer ID', width: 100 },
+        { field: 'Freight', headerText: 'Freight', textAlign: 'Center',format:'C2', width: 80 },
+        { field: 'ShipCity', headerText: 'Ship City', width: 100 }
     ],
     toolbar: ['Search'],
-    height: 270
+    height: 260
 });
 grid.appendTo('#Grid');
-
-
-
