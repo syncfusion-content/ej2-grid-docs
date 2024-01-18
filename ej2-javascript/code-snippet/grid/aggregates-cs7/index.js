7a76047bfd0ceaa6c3556cf870508402c5289af3
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2024-01-18 14:59:15 +0530

    EJ2-Documentation(865452) - UG Documentation of Group and Caption Aggregates, Custom Aggregates, Reactive Aggregates.

--- ej2-javascript/code-snippet/grid/aggregates-cs7/index.js
+++ ej2-javascript/code-snippet/grid/aggregates-cs7/index.js
@@ -3,8 +3,7 @@ ej.grids.Grid.Inject(ej.grids.Page, ej.grids.Toolbar, ej.grids.Edit, ej.grids.Ag
 var grid = new ej.grids.Grid({
     dataSource: data,
     allowPaging:true,
-    pageSettings:{pageSize:7},
-    toolbar: ['Edit', 'Delete', 'Update', 'Cancel'],
+    toolbar: ['Delete', 'Update', 'Cancel'],
     editSettings: { allowEditing: true, allowDeleting: true, mode: 'Normal' },
     actionBegin: function(args){
         if(args.requestType === 'beginEdit'){
@@ -13,12 +12,27 @@ var grid = new ej.grids.Grid({
         };
     },
     columns: [
-        { field: 'OrderID', headerText: 'Order ID', isPrimaryKey:true, textAlign: 'Right', width: 120 },
-        { field: 'CustomerID', headerText: 'Customer ID', width: 150 },
-        { field: 'Freight', headerText: 'Freight', editType: 'numericedit', format: 'C2', edit: { params: { change: funChange } },
-        width: 150},
-        { field: 'ShipCountry', headerText: 'Ship Name', width: 150 }
-    ],
+        { field: "OrderID", headerText: "Order ID", isPrimaryKey: true, textAlign: "Right", width: 120 },
+        { field: "CustomerID", headerText: "Customer ID", width: 150 },
+        { field: "OrderDate", headerText: "OrderDate", width: 120, format: "yMd" },
+        {
+          field: "Freight",
+          headerText: "Freight",
+          editType: "numericedit",
+          format: "C2",
+          edit: {
+            params: {
+              change: function (args) {
+                selectedRecords["Freight"] = args.value; // Set the edited value to aggregate column
+                grid.aggregateModule.refresh(selectedRecords); // Refresh aggregates using edited data
+              },
+            },
+          },
+          width: 150,
+        },
+        { field: "ShipCountry", headerText: "Ship Country", width: 150 },
+        { field: "ShipCity", headerText: "Ship City", width: 150 }
+      ],
     height: 268,
     aggregates: [{
         columns:[{
@@ -30,10 +44,3 @@ var grid = new ej.grids.Grid({
     }]
 });
 grid.appendTo('#Grid');
-
-function funChange(args){
- let gridObj = document.getElementById('Grid')['ej2_instances'][0];
- selectedRecords['Freight'] = args.value; // Set the edited value to aggregate column                 
- gridObj.aggregateModule.refresh(selectedRecords) // Refresh aggregates using edited data          
-}
-
