52012d744dc2b5b8340e0a6a2dbeb5c26b190ff3
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2024-01-23 14:09:24 +0530

    EJ2-Documentation(854125) - UG Documentation for filtering

---
+++ ej2-javascript/code-snippet/grid/filtering-caseSensitivity-cs1/index.js
@@ -0,0 +1,23 @@
+ej.grids.Grid.Inject(ej.grids.Filter);
+var grid = new ej.grids.Grid({
+    dataSource: data,
+    allowFiltering: true,
+    filterSettings : { enableCaseSensitivity: false },
+    columns: [
+        { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 90 },
+        { field: 'CustomerID', headerText: 'Customer ID', width: 100 },
+        { field: 'ShipCountry', headerText: 'Ship Country', width: 90 },
+        { field: 'ShipCity', headerText: 'Ship City', width: 120 },
+        { field: 'ShipRegion', headerText: 'Ship Region', width: 120 }
+    ]
+});
+grid.appendTo('#Grid');
+
+var toggle = new ej.buttons.Switch({
+    change: onToggleCaseSensitive,
+});
+toggle.appendTo('#switch');
+
+function onToggleCaseSensitive(args) {
+       grid.filterSettings.enableCaseSensitivity = args.checked;
+}
