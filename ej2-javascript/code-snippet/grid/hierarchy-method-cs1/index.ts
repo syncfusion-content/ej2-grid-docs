8402ef7d5d67b556d00fbf637f5707f5ed70dd7e
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2024-01-25 10:45:49 +0530

    EJ2-Documentation(851668) - UG Documentation for Hierarchy grid

--- ej2-javascript/code-snippet/grid/hierarchy-method-cs1/index.ts
+++ ej2-javascript/code-snippet/grid/hierarchy-method-cs1/index.ts
@@ -1,45 +1,40 @@
-
-
 import { Grid, DetailRow } from '@syncfusion/ej2-grids';
 import { Button } from '@syncfusion/ej2-buttons';
 import { data, employeeData } from './datasource.ts';
 
 Grid.Inject(DetailRow);
 
-let expandBtn: Button = new Button();
-expandBtn.appendTo('#expandall');
+let expandButton: Button = new Button();
+expandButton.appendTo('#expandall');
 
-let collapseBtn: Button = new Button();
-collapseBtn.appendTo('#collapseall');
+let collapseButton: Button = new Button();
+collapseButton.appendTo('#collapseall');
 
 let grid: Grid = new Grid({
     dataSource: employeeData,
     columns: [
-        { field: 'EmployeeID', headerText: 'Employee ID', textAlign: 'Right', width: 120 },
-        { field: 'FirstName', headerText: 'First Name', width: 150 },
-        { field: 'City', headerText: 'City', width: 150 },
-        { field: 'Country', headerText: 'Country', width: 150 }
+      { field: 'EmployeeID', headerText: 'Employee ID', textAlign: 'Right', width: 100 },
+      { field: 'FirstName', headerText: 'First Name', width: 100 },
+      { field: 'LastName', headerText: 'LastName', width: 100 },
+      { field: 'City', headerText: 'City', width: 120 }
     ],
     childGrid: {
         dataSource: data,
         queryString: 'EmployeeID',
         columns: [
-            { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 120 },
-            { field: 'CustomerID', headerText: 'Customer ID', width: 150 },
-            { field: 'ShipCity', headerText: 'Ship City', width: 150 },
-            { field: 'ShipName', headerText: 'Ship Name', width: 150 }
+            { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 90 },
+            { field: 'CustomerID', headerText: 'Customer ID', width: 100 },
+            { field: 'ShipCity', headerText: 'Ship City', width: 100 },
+            { field: 'ShipName', headerText: 'Ship Name', width: 100 }
         ],
     },
     height: 265
 });
 grid.appendTo('#Grid');
 
-document.getElementById('expandall').addEventListener('click', () => {
+(document.getElementById('expandall')as HTMLElement).addEventListener('click', () => {
     grid.detailRowModule.expandAll();
 });
-document.getElementById('collapseall').addEventListener('click', () => {
+(document.getElementById('collapseall')as HTMLElement).addEventListener('click', () => {
     grid.detailRowModule.collapseAll();
 });
-
-
-
