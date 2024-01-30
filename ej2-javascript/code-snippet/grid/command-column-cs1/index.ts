269dd29cf8d2d7869a1386063b3978e5eadcf846
harinisf <121146560+harinisf@users.noreply.github.com>
2024-01-30 18:02:59 +0530

    aligned the code in typescript file

--- ej2-javascript/code-snippet/grid/command-column-cs1/index.ts
+++ ej2-javascript/code-snippet/grid/command-column-cs1/index.ts
@@ -9,10 +9,8 @@ let grid: Grid = new Grid({
     columns: [
       { field: 'OrderID', isPrimaryKey: true, headerText: 'Order ID', textAlign: 'Right', width: 100 },
       { field: 'CustomerID', headerText: 'Cusomer ID', width: 120 },
-      { field: 'Freight', headerText: 'Freight',textAlign: 'Right',
-      editType: 'numericedit', width: 120, format: 'C2' },
-      { field: 'ShipCountry', headerText: 'Ship Country', width: 150,textAlign: 'Right',
-      editType: 'dropdownedit'},
+      { field: 'Freight', headerText: 'Freight',textAlign: 'Right', editType: 'numericedit', width: 120, format: 'C2' },
+      { field: 'ShipCountry', headerText: 'Ship Country', width: 150,textAlign: 'Right', editType: 'dropdownedit'},
       { headerText: 'Commands', width: 120, 
       commands: [{ type: 'Edit', buttonOption: { cssClass: 'e-flat', iconCss: 'e-edit e-icons' } },
       { type: 'Delete', buttonOption: { cssClass: 'e-flat', iconCss: 'e-delete e-icons' } },
