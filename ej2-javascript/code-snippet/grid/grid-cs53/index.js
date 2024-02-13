9e521f3518bf85edaf2506b53cbbbb867869c9cf
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2024-02-13 18:35:12 +0530

    EJ2-Documentation(870556) - UG Documentation for excel-export options

--- ej2-javascript/code-snippet/grid/grid-cs53/index.js
+++ ej2-javascript/code-snippet/grid/grid-cs53/index.js
@@ -4,34 +4,21 @@ var grid = new ej.grids.Grid({
         allowPdfExport: true,
         allowExcelExport: true,
         allowPaging: true,
-        allowFiltering: true,
-        selectionSettings: {type: 'Multiple'},
-        toolbar: ['PdfExport','ExcelExport'],
-        pageSettings: { pageCount: 5, pageSize: 5 },
+        toolbar: ['ExcelExport'],
+        selectionSettings: { type: 'Multiple', enableSimpleMultiRowSelection: true},
         columns: [
             { field: 'OrderID', headerText: 'Order ID', width: 120, textAlign: 'Right' },
             { field: 'CustomerID', headerText: 'Customer ID', width: 150 },
-            { field: 'OrderDate', headerText: 'Order Date', width: 130, format: 'yMd', textAlign: 'Right' },
-            { field: 'Freight', width: 120, format: 'C2', textAlign: 'Right' },
-            { field: 'ShipCountry', visible: false, headerText: 'Ship Country', width: 150 }
+            { field: 'ShipCity', headerText: 'Ship City', width: 150 },
         ],
     });
 grid.appendTo('#Grid');
 grid.toolbarClick = (args) => {
-    if (args.item.id === 'Grid_pdfexport') {
-        let pdfdata = grid.getSelectedRecords();
-            let exportProperties = {
-                dataSource: pdfdata,
-            };
-            grid.pdfExport(exportProperties);
-        }
-    else if (args.item.id === 'Grid_excelexport') {
-        let exceldata = grid.getSelectedRecords();
-            let exportProperties = {
-                dataSource: exceldata,
-            };
-            grid.excelExport(exportProperties);
+    if (args.item.id === 'Grid_excelexport') {
+       var selectedRecords =grid.getSelectedRecords();
+       var exportProperties = {
+            dataSource: selectedRecords
+        };
+       grid.excelExport(exportProperties);
         }
     }
-
-
